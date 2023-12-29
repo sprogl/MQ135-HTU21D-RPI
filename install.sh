b803033bed04534bd9f30f5378818c07dc37239d
sprogl <vahidtoomani2002@yahoo.com>
2023-12-29 18:25:07 +0100

    Beatify the install script

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+########################################
+echo 'Copy files...'
 cp -rfv app/ /root/app
 cp -rfv scripts/* /root/app/
 cp -rfv systemd/sensors.service /etc/systemd/system/sensors.service
-
+#########################################
+echo 'Setup the web service...'
 systemctl daemon-reload
 systemctl enable sensors.service
-
+#########################################
+echo 'Prepare the required modules...'
 python3 -m pip install --upgrade pip
 pip3 install --upgrade -r requirements.txt
-
+#########################################
+echo 'Start the service...'
 systemctl start sensors.service
